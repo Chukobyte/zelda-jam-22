6c362b1bb75db710b0c2236bc3ac28a0fdd4b07f
chukobyte <chukobyte@gmail.com>
2022-01-08 21:27:37 -0500

    Initial project set up.

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from seika.node import Node2D
+from seika.input import Input
+from seika.engine import Engine
+from seika.audio import AudioStream
+
+
+class Main(Node2D):
+    def _start(self) -> None:
+        print("In main!")
+        music_audio_stream = AudioStream.get(stream_uid="test-music")
+        music_audio_stream.play()
+
+    def _physics_process(self, delta: float) -> None:
+        if Input.is_action_just_pressed(action_name="debug_quit"):
+            Engine.exit()
