07d050520fd7310741ef1b576c283316cc037966
chukobyte <chukobyte@gmail.com>
2022-01-10 19:57:25 -0500

    Added credits screen.

---
+++ src/credits_screen.py
@@ -0,0 +1,19 @@
+from seika.node import Node2D
+from seika.scene import SceneTree
+from seika.input import Input
+from seika.engine import Engine
+from seika.utils import SimpleTimer
+
+from src.game_context import GameContext, GameState
+
+
+class CreditsScreen(Node2D):
+    def _physics_process(self, delta: float) -> None:
+        if Input.is_action_pressed(action_name="debug_quit"):
+            Engine.exit()
+
+        if Input.is_action_just_pressed(action_name="attack") or Input.is_action_just_pressed(
+            action_name="credits"
+        ):
+            GameContext.set_game_state(GameState.TITLE_SCREEN)
+            SceneTree.change_scene(scene_path="scenes/title_screen.sscn")
--- src/title_screen.py
+++ src/title_screen.py
@@ -8,9 +8,13 @@ from src.game_context import GameContext, GameState
 
 class TitleScreen(Node2D):
     def _physics_process(self, delta: float) -> None:
-        if Input.is_action_pressed(action_name="debug_exit"):
+        if Input.is_action_just_pressed(action_name="debug_quit"):
             Engine.exit()
 
-        if Input.is_action_pressed(action_name="attack"):
+        if Input.is_action_just_pressed(action_name="credits"):
+            GameContext.set_game_state(GameState.CREDITS)
+            SceneTree.change_scene(scene_path="scenes/credits_screen.sscn")
+
+        if Input.is_action_just_pressed(action_name="attack"):
             GameContext.set_game_state(GameState.PLAYING)
             SceneTree.change_scene(scene_path="scenes/main.sscn")
