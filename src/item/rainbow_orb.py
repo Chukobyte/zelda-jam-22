dbaf46450f4114db317f997412bcd66b22b7b7c8
chukobyte <chukobyte@gmail.com>
2022-01-12 18:40:47 -0500

    Added task to change color for rainbow orbs.

--- src/item/rainbow_orb.py
+++ src/item/rainbow_orb.py
@@ -1,12 +1,25 @@
 from seika.assets import Texture
+from seika.color import Color
 from seika.math import Rect2
 from seika.node import CollisionShape2D, Sprite
+
+from src.task.task import co_suspend, Task, co_wait_until_seconds, TaskManager
 
 
 class RainbowOrb(CollisionShape2D):
     def __init__(self, entity_id: int):
         super().__init__(entity_id)
         self.sprite = None
+        self.color_ramp = [
+            Color(255 / 255.0, 97 / 255.0, 178 / 255.0),
+            Color(255 / 255.0, 162 / 255.0, 0),
+            Color(235 / 255.0, 211 / 255.0, 32 / 255.0),
+            Color(154 / 255.0, 235 / 255.0, 0),
+            Color(97, 211, 227 / 255.0),
+            Color(162 / 255.0, 113 / 255.0, 255 / 255.0),
+            Color(243 / 255.0, 97 / 255.0, 255 / 255.0),
+        ]
+        self.tasks = TaskManager(initial_tasks=[Task(name="change_color", func=self.change_colors)])
 
     def _start(self) -> None:
         self.sprite = Sprite.new()
@@ -17,4 +30,15 @@ class RainbowOrb(CollisionShape2D):
         self.tags = ["rainbow_orb"]
 
     def _physics_process(self, delta: float) -> None:
-        pass
+        self.tasks.run_tasks()
+
+    @Task.task_func(debug=True)
+    def change_colors(self):
+        current_color_ramp = []
+        while True:
+            if len(current_color_ramp) <= 0:
+                current_color_ramp = self.color_ramp.copy()
+            new_color = current_color_ramp.pop(0)
+            self.sprite.modulate = new_color
+            yield from co_wait_until_seconds(wait_time=0.2)
+        yield co_suspend()
