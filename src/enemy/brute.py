23208b424a5018450ad36531908691c92c17251c
chukobyte <chukobyte@gmail.com>
2022-01-14 01:03:07 -0500

    Clean.

--- src/enemy/brute.py
+++ src/enemy/brute.py
@@ -6,6 +6,7 @@ from seika.physics import Collision
 from seika.utils import SimpleTimer
 
 from src.enemy.enemy import Enemy
+from src.math.ease import Ease
 from src.task.task import Task, co_wait_until_seconds, co_return, co_suspend
 from src.world import World
 
@@ -29,25 +30,31 @@ class Brute(Enemy):
         world = World()
         player = self.get_node(name="Player")
         assert player
-        move_speed = 50
+        move_speed = 90
         while True:
             yield from co_wait_until_seconds(wait_time=random.uniform(2.0, 4.0))
             rand_dirs = [Vector2.DOWN(), Vector2.UP(), Vector2.LEFT(), Vector2.RIGHT()]
             random.shuffle(rand_dirs)
             new_dir = rand_dirs.pop(0)
             move_timer = SimpleTimer(
-                wait_time=random.uniform(0.5, 1.5), start_on_init=True
+                wait_time=random.uniform(0.9, 1.3), start_on_init=True
             )
+            elapsed_time = 0.0
             while not move_timer.tick(delta=world.cached_delta):
+                elapsed_time += world.cached_delta
                 new_vel = Vector2(
                     new_dir.x * move_speed * world.cached_delta,
                     new_dir.y * move_speed * world.cached_delta,
                 )
+                new_pos = new_vel + self.position
+                new_pos = Ease.Cubic.ease_out_vec2(elapsed_time=elapsed_time, from_pos=self.position, to_pos=new_pos, duration=move_timer.wait_time)
                 collisions = Collision.get_collided_nodes_by_tag(
                     node=self.collider, tag="solid", offset=new_vel
                 )
                 if not collisions:
-                    self.position += new_vel
+                    self.position = new_pos
+                elif len(rand_dirs) > 0:
+                    new_dir = rand_dirs.pop(0)
                 yield co_suspend()
         yield co_return()
 
