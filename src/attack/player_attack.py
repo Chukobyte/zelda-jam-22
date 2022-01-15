b66e59675c8d7db4e2a09299552b876682887e0d
chukobyte <chukobyte@gmail.com>
2022-01-14 23:08:18 -0500

    Added bomb explosion.

--- src/attack/player_attack.py
+++ src/attack/player_attack.py
@@ -66,6 +66,37 @@ class BoltAttack(Attack):
             first_enemy.take_damage(attack=self)
 
 
+class BombExplosion(Attack):
+    def __init__(self, entity_id: int):
+        super().__init__(entity_id)
+        self.sprite = None
+        self.set_life_time(2.0)
+        self.damage = 2
+
+    def _start(self) -> None:
+        super()._start()
+        self.sprite = Sprite.new()
+        texture = Texture.get(
+            file_path="assets/images/player/player_bomb_explosion.png"
+        )
+        self.sprite.texture = texture
+        self.sprite.draw_source = Rect2(0, 0, 48, 48)
+        self.add_child(self.sprite)
+
+        self.collider_rect = Rect2(0, 0, 48, 48)
+        Audio.play_sound(sound_id="assets/audio/sfx/bomb_explosion.wav")
+
+    def _physics_process(self, delta: float) -> None:
+        super()._physics_process(delta)
+        enemies_colliders = Collision.get_collided_nodes_by_tag(
+            node=self, tag=Enemy.TAG
+        )
+        if enemies_colliders and not self.has_collided:
+            self.has_collided = True
+            first_enemy = enemies_colliders[0].get_parent()
+            first_enemy.take_damage(attack=self)
+
+
 class BombAttack(Attack):
     def __init__(self, entity_id: int):
         super().__init__(entity_id)
@@ -81,16 +112,14 @@ class BombAttack(Attack):
         self.add_child(self.sprite)
 
         self.collider_rect = Rect2(0, 0, texture.width, texture.height)
+        self.position_spawned = self.position
 
+    # TODO: There is a bug with grabbing component values for at least transformation in thr _end function.
+    #  Components should still be accessible during this time.
     def _physics_process(self, delta: float) -> None:
-        super()._physics_process(delta)
-        enemies_colliders = Collision.get_collided_nodes_by_tag(
-            node=self, tag=Enemy.TAG
-        )
-        if enemies_colliders and not self.has_collided:
-            self.has_collided = True
-            first_enemy = enemies_colliders[0].get_parent()
-            first_enemy.take_damage(attack=self)
-
-    def _end(self) -> None:
-        Audio.play_sound(sound_id="assets/audio/sfx/bomb_explosion.wav")
+        if self.life_timer.tick(delta):
+            bomb_explosion = BombExplosion.new()
+            bomb_explosion.position = self.position + Vector2(-16, -16)
+            main_node = self.get_node(name="Main")
+            main_node.add_child(bomb_explosion)
+            self.queue_deletion()
