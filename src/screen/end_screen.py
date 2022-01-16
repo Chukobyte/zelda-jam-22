7dd66439d4c98166dcce335418f3ae3ff70bff97
chukobyte <chukobyte@gmail.com>
2022-01-16 10:25:34 -0500

    Clean up.

--- src/screen/end_screen.py
+++ src/screen/end_screen.py
@@ -20,6 +20,7 @@ class EndScreen(Node2D):
         play_time_label.text = (
             f"Play Time: {game_context.play_time_counter.time_played_text}"
         )
+        Audio.stop_music()
 
     def _physics_process(self, delta: float) -> None:
         if Input.is_action_just_pressed(action_name="debug_quit"):
